cd278d371431e67528b8c8c84a37233d3301fd8f
AgusT <agusterre01@gmail.com>
2024-12-22 20:06:01 -0300

    Implementacion de Arrays y mejoras en las funciones

--- scripts/agendar.js
+++ scripts/agendar.js
@@ -2,35 +2,80 @@ let tasks = {};
 
 let days = document.querySelectorAll('.date div');
 
+/*FUNCION PARA AÑADIR TAREAS*/
+
 function addTask(day) {
-  if (day.textContent.trim() !== '') {
-    let task = prompt('Escribe una tarea para el día ' + day.textContent + ':');
-    
-    if (task && task.trim() !== '') {
-      if (!tasks[day.textContent]) {
-        tasks[day.textContent] = '';
+  if (day.textContent.trim() !== "") {
+    let task = prompt("Ingrese una tarea: ");
+
+    if (task !== null) {
+      task = task.trim();
+
+      if (task !== "") {
+        if (!tasks[day.dataset.day]) {
+          tasks[day.dataset.day] = [];
+        }
+
+        if (tasks[day.dataset.day].includes(task)) {
+          alert("Ya has agregado esta tarea.");
+          return;
+        }
+
+        tasks[day.dataset.day].push(task);
+
+        /*LE AGREGUE UN BOTON PARA MOSTRAR LAS TAREAS UNA VEZ CREADA LA PRIMER TAREA*/
+
+        if (!day.querySelector('.task-button')) {
+          let taskButton = document.createElement('button');
+          taskButton.classList.add('task-button');
+          taskButton.textContent = "Ver Tareas";
+
+          /*AL DARLE CLICK AL BOTON SE EJECUTA LA FUNCION "SHOWTASKS"*/
+
+          taskButton.onclick = function (event) {
+            event.stopPropagation();
+            showTasks(day);
+          };
+
+          day.appendChild(taskButton);
+        }
+
+      } else {
+        alert("No ingresaste ninguna tarea");
       }
-      tasks[day.textContent] += (tasks[day.textContent] ? '\n' : '') + task;
+    } else {
+      alert("Operación cancelada");
     }
   }
-  showTasks(day);
 }
 
-function showTasks(day) {
-  let taskText = tasks[day.textContent];
+/*FUNCION PARA MOSTRAR LAS TAREAS GUARDADAS*/
 
-  if (taskText && taskText.trim() !== '') {
-    alert('Tareas del día ' + day.textContent + ':\n' + taskText);
+function showTasks(day) {
+  if (tasks[day.dataset.day] && tasks[day.dataset.day].length > 0) {
+    alert("Tareas del día " + day.dataset.day + " :\n" + tasks[day.dataset.day].join("\n"));
   } else {
-    alert('No hay tareas para el día ' + day.textContent);
+    alert("No hay tareas para este día.");
   }
 }
 
-for (let i = 0; i < days.length; i++) {
-  days[i].onclick = function () {
-    addTask(days[i]);
-  };
+/*AL DARLE CLICK A UN DIA SE EJECUTA LA FUNCION ADDTASK*/
+
+for (const elemento of days) {
+  elemento.dataset.day = elemento.textContent.trim();
+
+  elemento.addEventListener('click', function(event) {
+    addTask(elemento);
+  });
 }
+
+
+
+
+
+
+
+
 
 
 
